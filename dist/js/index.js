7e2a1ceb4bf0c374bd63c6b505f19dcb6b5b9957
thiagotnaraujo <thago@crowdability.com>
2025-03-10 15:20:56 -0300

    Project restart

--- dist/js/index.js
+++
@@ -1,5 +0,0 @@
-import LibraryServices from "./src/services/LIbraryServices.js";
-let libraryUrl = 'http://localhost:3000/Library/books';
-const library = new LibraryServices();
-const allBooks = library.fetchData(libraryUrl);
-console.log(allBooks);
