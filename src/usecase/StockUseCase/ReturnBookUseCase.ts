ba2b9081bacb1bd00094995b19358d8ca27fb659
thiagotnaraujo <thago@crowdability.com>
2025-03-19 10:27:23 -0300

    Return Book hotfix

--- src/usecase/StockUseCase/ReturnBookUseCase.ts
+++ src/usecase/StockUseCase/ReturnBookUseCase.ts
@@ -1,4 +1,6 @@
+import IStock from '@/interfaces/IStock'
 import IStockRepository from '@/interfaces/IStockRepository'
+import GetBookByFromStockUseCase from './GetBookFromStockUseCase'
 
 export default class ReturnBookUseCase {
   constructor(private stockRepository: IStockRepository) {}
@@ -7,6 +9,16 @@ export default class ReturnBookUseCase {
     console.log('Returning book:', bookId)
 
     try {
+      const getBookFromStockUseCase = new GetBookByFromStockUseCase(
+        this.stockRepository,
+      )
+      const book = await getBookFromStockUseCase.execute(bookId)
+
+      if (book.borrowed < 1) {
+        console.log('No borrowed copies to return')
+        return
+      }
+
       await this.stockRepository.returnBook(bookId)
       console.log('Book returned successfully')
     } catch (error) {
